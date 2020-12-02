8761e49ea86207f0d0ce55967c84bfea588fdba3
long0204 <longpdh02@gmail.com>
2020-12-03 00:12:56 +0700

    upload

--- 01/app/src/main/java/com/example/alarmy/MainActivity.java
+++ 01/app/src/main/java/com/example/alarmy/MainActivity.java
@@ -5,10 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btncheck1;
     @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btncheck1 = (checkbox) findViewById(R.id.checktime1);
+        btncheck1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mh2 = new Intent(MainActivity.this, activity_checktime1.class);
+                startActivity(mh2);
+            }
+        });
+    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        }
+
+
 }
